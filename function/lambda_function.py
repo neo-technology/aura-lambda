9e21a04b7cd4d5d5e399e02c9fb9a2363578c640
Moxious <moxious@oldhat.org>
2021-10-08 09:34:02 -0400

    incremental work

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -4,6 +4,7 @@ import jsonpickle
 import json
 import boto3
 from aws_xray_sdk.core import patch_all
+import uuid
 import os
 from time import time
 from neo4j import GraphDatabase
@@ -29,26 +30,49 @@ client.get_account_settings()
 
 milliseconds = lambda: int(time() * 1000)
 
+tracking = {
+    "lambda_id": str(uuid.uuid4()),
+    "lambda_start": milliseconds(),
+    "job_start": -1,
+    "job_end": -1,
+    "job_rate": -1
+}
+
 def write_graph_record(tx, event):
     json_messages = [json.loads(msg['body']) for msg in event['Records']]
 
-    start = milliseconds()
+    logging.info(json.dumps(json_messages))
+    tracking["job_start"] = milliseconds()
+
+    # CREATE INDEX ON :Person(first_name);
+    # CREATE INDEX ON :Person(last_name);
+    # CREATE INDEX ON :Company(name);
+    # CREATE INDEX ON :Country(name);
+    # CREATE INDEX ON :Job(name);
 
     try:
         tx.run("""
-            UNWIND $json_messages AS message
-            MERGE (p:Person { name: message.name })
+            CREATE (batch:Batch { 
+                time: datetime(), 
+                size: size($json_messages)
+            }) SET batch += $tracking
+            WITH $json_messages AS messages
+            UNWIND messages AS message
+            MERGE (p:Person { first_name: message.first_name, last_name: message.last_name })
             MERGE (c:Company { name: message.company })
             MERGE (co:Country { name: message.country })
+            MERGE (j:Job { name: message.job })
             MERGE (p)-[:WORKS_FOR]->(c)
-            MERGE (c)-[:IN]->(co);
-        """, json_messages=json_messages)
+            MERGE (p)-[:HAS]->(j)
+            MERGE (c)-[:IN]->(co)
+        """, json_messages=json_messages, tracking=tracking)
     except Exception as e:
         raise e
     finally:
-        delta = milliseconds() - start
-        rt = len(json_messages) / (delta/1000)
-        logger.info('Wrote batch of %d graph records in %d ms or %f recs/sec avg' % (len(json_messages), delta, rt))
+        tracking["job_end"] = milliseconds()
+        delta = tracking["job_end"] - tracking["job_start"]
+        tracking["job_rate"] = len(json_messages) / (delta/1000)
+        logger.info('Wrote batch of %d graph records in %d ms or %f recs/sec avg' % (len(json_messages), delta, tracking["job_rate"]))
 
 def lambda_handler(event, context):
     #logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
--- message_faker/main.py
+++ message_faker/main.py
@@ -2,7 +2,7 @@ import queue_wrapper
 from faker import Faker
 import json
 import message_wrapper
-from time import time
+from time import time, sleep
 
 fake = Faker()
 
@@ -14,9 +14,11 @@ milliseconds = lambda: int(time() * 1000)
 def make_message(i):
     return {
         'body': json.dumps({ 
-            "name": fake.name(),
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
             "country": fake.country(),
             "company": fake.company(),
+            "job": fake.job(),
             "index": i,
         }),
         'attributes': {
@@ -27,6 +29,7 @@ def make_message(i):
 start = milliseconds()
 
 while True:
+    sleep(0.25)
     batch = [make_message(i) for i in range(0, 10)]
     message_wrapper.send_messages(q, batch)
     msgs = msgs + len(batch)
